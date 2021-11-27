b7237f030965d1736d6c1a81ead12361608490c0
Andrei Cioc <andrei.cioc@logit-solutions.com>
2021-11-27 16:00:25 +0200

    v1.6

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -3,6 +3,12 @@ import { Row, Col , Button , Form} from 'react-bootstrap'
 
 function ContactForm() {
 
+    const encode = (data) => {
+        return Object.keys(data)
+            .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+            .join("&");
+      }
+
     const handleChange = e => setFormValues({ ...formValues, [e.target.name]: e.target.value })
         const [formValues, setFormValues] = useState({
         email:'',
@@ -10,11 +16,29 @@ function ContactForm() {
         message: ''
     })
 
+    const handleSubmit = e => {
+            fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact"})
+            })
+            .then(() => alert("Success!"))
+            .catch(error => alert(error));
+    
+            e.preventDefault();
+        };
+
     return (
         <Row className="rows">
             <Col Col xs={12} sm={12} md={12} lg={12} xxl={12} className="centeredColumn">
-                <Form action="/contact" name="contact" method="POST" className = "contactForm" data-netlify="true">
+                <Form action="/contact" name="contact" method="POST" data-netlify="true" className = "contactForm" onSubmit={handleSubmit}>
                 <input type="hidden" name="form-name" value="contact" />
+                    {/* Name */}
+                    <Form.Group className="mb-3" controlId="formName">
+                        <Form.Label>Full Name</Form.Label>
+                        <Form.Control type="input" placeholder="Full Name" className = "formRows" onChange={handleChange}/>
+                    </Form.Group>
+                    {/* Email */}
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
                         <Form.Control type="email" placeholder="Enter email" className = "formRows" onChange={handleChange}/>
@@ -22,11 +46,7 @@ function ContactForm() {
                         We'll never share your email with anyone else.
                         </Form.Text>
                     </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="formName">
-                        <Form.Label>Full Name</Form.Label>
-                        <Form.Control type="input" placeholder="Full Name" className = "formRows" onChange={handleChange}/>
-                    </Form.Group>
+                    {/* Password */}
                     <Form.Group className="mb-3" controlId="formDesc">
                         <Form.Label>Message</Form.Label>
                         <Form.Control as="textarea" rows={4} className = "formRows" style={{fontSize:'1.1rem'}} onChange={handleChange}/>
